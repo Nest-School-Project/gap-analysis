461e252e928efa4e59fe752904557bec23bc721e
Anudhina <anudhina03@gmail.com>
2023-09-07 14:28:13 +0530

    anuu

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
      </Routes>
      </BrowserRouter>
       {/* <Login></Login> */}
+      
     </div>
   );
 }
---
+++ src/gap_analysis.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const gap_analysis = () => {
+  return (
+    <div>
+        <h1>GAP ANALYSIS</h1>
+    </div>
+  )
+}
