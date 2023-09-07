666c41341dcbb832b52d32aade8b80a7f522bd58
adithya <adithyajawaher@gmail.com>
2023-09-07 14:20:52 +0530

    grades

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 //import logo from './logo.svg';
 import './App.css';
+import './Grades.css';
 import Header from './Components/Header';
 import { Login } from './Login';
+import { Grades } from './Grades';
+
 
 function App() {
   return (
     <div className="App">
      
       <Login></Login>
+      <Grades></Grades>
     </div>
   );
 }
---
+++ src/Boxes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function Boxes(p) {
+  return (
+  
+    <div>
+        
+
+        
+    
+    <div class="box" style={{display:"flexbox"}}>
+  <h1 >{p.Names}</h1>
+  </div>
+    </div>
+    
+  )
+}
+
+export default Boxes
