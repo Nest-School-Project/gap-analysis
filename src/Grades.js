b8d04ad0518b29feb5a4a680ac49e9f0c6d02e2b
adithya <adithyajawaher@gmail.com>
2023-09-07 15:09:08 +0530

    grade over

--- src/Grades.js
+++ src/Grades.js
@@ -12,7 +12,12 @@ export const Grades = () => {
     <Boxes Names="1"></Boxes>
     <Boxes Names="2"></Boxes>
     <Boxes Names="3"></Boxes>
-    {/* <Boxes Names=""></Boxes> */}
+   <Boxes Names="4"></Boxes>
+    <Boxes Names="5"></Boxes>
+    <Boxes Names="6"></Boxes>
+    <Boxes Names="7"></Boxes>
+    <Boxes Names="8"></Boxes>
+    
     </div>
     </div>
   )
