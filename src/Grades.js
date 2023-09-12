b49402eeec3c5bb9f9d701a5ed125103e9f4309c
Aravind2203 <aravindofficial2203@gmail.com>
2023-09-12 14:43:04 +0530

    Grades list

--- src/Grades.js
+++ src/Grades.js
@@ -12,22 +12,29 @@ export const Grades = () => {
     console.log(e)
     navigate(`/class-details/${e}`)
   };
+  const grades=[
+    "LKG",
+    "UKG",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8"
+  ]
   return(<div>
     <Header />
     <h1>Grades </h1>
     <div className='Boxstyle'>
-    <button onClick={()=>handleclick("LKG")} style={{margin:"30px"}}><Boxes Names="LKG"></Boxes></button>
-    <button  onClick={()=>handleclick("UKG")} style={{margin:"30px"}}> <Boxes Names="UKG"></Boxes></button>
-    <button onClick={()=>handleclick("1")} style={{margin:"30px"}}> <Boxes Names="1"></Boxes></button>
-    <button onClick={()=>handleclick("2")} style={{margin:"30px"}}> <Boxes Names="2"></Boxes></button>
-    <button onClick={()=>handleclick("3")} style={{margin:"30px"}}><Boxes Names="3"></Boxes></button>
-    <button onClick={()=>handleclick("4")} style={{margin:"30px"}}> <Boxes Names="4"></Boxes></button>
-    <button onClick={()=>handleclick("5")} style={{margin:"30px"}}><Boxes Names="5"></Boxes></button>
-    <button onClick={()=>handleclick("6")} style={{margin:"30px"}}> <Boxes Names="6"></Boxes></button>
-    <button onClick={()=>handleclick("7")} style={{margin:"30px"}}> <Boxes Names="7"></Boxes></button>
-    <button onClick={()=>handleclick("8")} style={{margin:"30px"}}><Boxes Names="8"></Boxes></button>
-    
-    
+    {
+      grades.map((grade,index)=>{
+        return (
+          <button onClick={()=>handleclick(grade)} style={{margin:"30px"}}><Boxes Names={grade}></Boxes></button>
+        )
+      })
+  }  
     </div>
     </div>
   )
