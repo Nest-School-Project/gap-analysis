11c8925583be8b9904324485cf1e997d382c2f5a
name <anirudh9707@gmail.com>
2023-09-20 11:36:01 +0530

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import menu3 from "./menu3.png"
-import { Link } from 'react-router-dom';
 
 import logo from "./logo2.svg"
 import SideBar from './SideBar'
- 
+import { useNavigate } from 'react-router-dom'; 
 const Header = () => {
 // JavaScript to toggle the hamburger state
 // JavaScript to toggle the hamburger state
+let Navigate=useNavigate();
+const handleLogout = () => {
+  Navigate('/')
+}
 return (
     <div>
      
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"></link>
 <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet"></link>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Acme"></link>
-<nav class="text-primary">
+ <nav class="text-primary">
 <div class="container-fluid">
 <SideBar></SideBar>
 <img src={logo} alt="nest" id="nestlogo" width="50" height="50"/>
-<button style={{height:"50px", width:"150px",color:"orange",position:"absolute",right:"5%",top:"5%",border-removeEventListener}}>LOGOUT</button>
+<button style={{height:"50px", width:"150px",color:"orange",position:"absolute",right:"5%",top:"5%",border:"solid orange 5px 5px",borderRadius:"30px 15px",boxShadow:"1px 1px 1px 1px"}} onClick={handleLogout}>LOGOUT</button>
 
 </div>
-</nav>
+ </nav>
 
 
 
